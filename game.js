b10b6d8e00e0311edf66f402dae19cc8cae1f29d
dand <79340018+dZyad@users.noreply.github.com>
2024-01-03 11:48:21 -0300

    Add computer random choice

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log(`Hello world`);
+// Game constants
+const ROCK = "rock";
+const PAPER = "paper"
+const SCISSORS = "scissors"
+
+/** 
+ *Generates randomly the computer choice in Rock Paper scissors game
+ * return {string}
+ */
+function getComputerChoice() {
+    let options = [ROCK, PAPER, SCISSORS];
+    let choice = Math.floor( Math.random() * options.length );
+    return options[choice];
+}
