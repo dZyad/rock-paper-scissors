3ea64c22e3d67a4b004d3bd96d82d7fc33fee5c3
dand <79340018+dZyad@users.noreply.github.com>
2024-01-03 13:44:14 -0300

    Add best of five type of Game

--- game.js
+++ game.js
@@ -29,27 +29,34 @@ function getPlayerChoice() {
  */
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        playerSelection = getPlayerChoice()
+        console.log("It's a tie, let's replay this round");
+        playerSelection = getPlayerChoice();
         computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
     }
     if (playerSelection === ROCK) {
         if( computerSelection === PAPER ) {
             console.log("You Lose! Paper beats Rock");
+            return "computer";
         } else {
             console.log("You Win! Rock beats Scissors");
+            return "player";
         }
     } else if (playerSelection === PAPER) {
         if ( computerSelection === SCISSORS ) {
             console.log("You Lose! Scissors beats Paper");
+            return "computer";
         } else {
             console.log("You Win! Paper beats Rock");
+            return "player";
         }
     } else if (playerSelection === SCISSORS) {
         if ( computerSelection === ROCK ) {
             console.log("You Lose! Rock beats Scissors");
+            return "computer";
         } else {
             console.log("You Win! Scissors beats Paper");
+            return "player";
         }
     } else {
         console.log("Please enter a valid value.")
@@ -57,4 +64,25 @@ function playRound(playerSelection, computerSelection) {
         computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
     }
+}
+
+function game() {
+    let isGameInProgress = true;
+    let winCount = {player: 0, computer: 0}
+
+    while(isGameInProgress) {
+        console.log(winCount);
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        const winner = playRound(playerSelection, computerSelection);
+        winCount[winner] ++;
+        if (winCount.player === 3) {
+            isGameInProgress = false;
+            console.log("Congrats! You've won!");
+        } else if ( winCount.computer === 3 ) {
+            isGameInProgress = false;
+            console.log("You've lost this time! Try again!");
+        }
+    }
+    console.log(winCount);
 }
