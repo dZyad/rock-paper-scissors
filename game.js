095c9c628c56aade863d8374e93a66a12c332cbc
dand <79340018+dZyad@users.noreply.github.com>
2024-01-10 17:59:19 -0300

    Add functionality to game
    Still missing some keys functionality that will be added later
    as limiting the amount of rounds that are played

--- game.js
+++ game.js
@@ -3,90 +3,61 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 const TIE = "tie";
-const WRONG_VALUE = "Wrong value";
+let winCount = {player: 0, computer: 0}
 
-/** 
- *Generates randomly the computer choice in Rock Paper scissors game
- * return {string}
- */
+let userScore = document.querySelector('#user-score');
+let computerScore = document.querySelector('#computer-score');
+userScore.textContent = winCount.player;
+computerScore.textContent = winCount.computer;
+
+document.addEventListener("click", function(event) {
+    let result = playRound(event.target.id, getComputerChoice());
+    updateWinCount(winCount, result);
+})
+
 function getComputerChoice() {
     let options = [ROCK, PAPER, SCISSORS];
     let choice = Math.floor( Math.random() * options.length );
     return options[choice];
 }
 
-/**
- * Ask User to make a choice of Rock paper or Scissors
- * @returns string
- */
-function getPlayerChoice() {
-    return prompt("What are you going to choose? Rock, Paper or Scissors?").toLowerCase();
-}
-
-/**
- * Allows Users to play a round of rock paper scissors
- * @param {string} playerSelection 
- * @param {string} computerSelection 
- * @returns 
- */
 function playRound(playerSelection, computerSelection) {
+    let lastRound = document.querySelector('.last-round');
     if (playerSelection === computerSelection) {
-        console.log("It's a tie, let's replay this round");
+        lastRound.textContent = "It's a tie, let's replay this round";
         return TIE;
     }
     if (playerSelection === ROCK) {
         if( computerSelection === PAPER ) {
-            console.log("You Lose! Paper beats Rock");
+            lastRound.textContent = "You Lose! Paper beats Rock";
             return "computer";
         } else {
-            console.log("You Win! Rock beats Scissors");
+            lastRound.textContent = "You Win! Rock beats Scissors";
             return "player";
         }
     } else if (playerSelection === PAPER) {
         if ( computerSelection === SCISSORS ) {
-            console.log("You Lose! Scissors beats Paper");
+            lastRound.textContent = "You Lose! Scissors beats Paper";
             return "computer";
         } else {
-            console.log("You Win! Paper beats Rock");
+            lastRound.textContent = "You Win! Paper beats Rock";
             return "player";
         }
     } else if (playerSelection === SCISSORS) {
         if ( computerSelection === ROCK ) {
-            console.log("You Lose! Rock beats Scissors");
+            lastRound.textContent = "You Lose! Rock beats Scissors";
             return "computer";
         } else {
-            console.log("You Win! Scissors beats Paper");
+            lastRound.textContent = "You Win! Scissors beats Paper";
             return "player";
         }
-    } else {
-        console.log("Please enter a valid value.")
-        return WRONG_VALUE;
     }
 }
 
-/**
- * Let user to play a Best of five game
- */
-function game() {
-    let isGameInProgress = true;
-    let winCount = {player: 0, computer: 0}
-
-    while(isGameInProgress) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-
-        const result = playRound(playerSelection, computerSelection);
-
-        if (result != TIE || result != WRONG_VALUE) {
-            winCount[result] ++;
-        }
-        if (winCount.player === 3) {
-            isGameInProgress = false;
-            console.log("Congrats! You've won!");
-        } else if ( winCount.computer === 3 ) {
-            isGameInProgress = false;
-            console.log("You've lost this time! Try again!");
-        }
+let updateWinCount = (winCount, result) => {
+    if (winCount != TIE) {
+        winCount[result]++;
     }
-    console.log(`Final result is Player: ${winCount.player} - Computer: ${winCount.computer}`);
+    userScore.textContent = winCount.player;
+    computerScore.textContent = winCount.computer;
 }
